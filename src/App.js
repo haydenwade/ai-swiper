f592df121c0d869c3a601c49174d1957c8a21527
haydenwade <hayden.wade@alula.net>
2020-05-21 21:47:09 -0600

    split screen, added prediction jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import { Container, Row, Col, Button, Carousel } from 'react-bootstrap';
+import { Container, Row, Col, Button, Carousel, Spinner,Jumbotron } from 'react-bootstrap';
 
 import { api } from './helpers/api';
 
@@ -46,26 +46,13 @@ export default class App extends React.Component {
             <hr />
           </Col>
         </Row>
+
         <Row>
           <Col className="text-center">
-            <Button onClick={this.dislike}>Dislike</Button>
-            <Button onClick={this.like}>Like</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {!this.state.loading && this.state.profiles.length > 0 &&
-              <React.Fragment>
-                {this.state.profiles[this.state.currentProfile].name}
-                <br />
-                {this.state.profiles[this.state.currentProfile].bio}
-              </React.Fragment>
-            }
-          </Col>
-        </Row>
-        <Row>
-          <Col className="text-center">
-            {this.state.loading && <h2>Loading...</h2>}
+            {this.state.loading && <React.Fragment>
+              <h2>Loading...</h2>
+              <Spinner animation="grow" />
+            </React.Fragment>}
             {!this.state.loading && this.state.profiles.length > 0 &&
               <Carousel>
                 {this.state.profiles[this.state.currentProfile].photos.map((p, i) => {
@@ -79,6 +66,37 @@ export default class App extends React.Component {
                 }
               </Carousel>
             }
+          </Col>
+          <Col>
+            <Row>
+              <Col className="text-center">
+                <Button onClick={this.dislike} style={{ width: '100%' }}>Dislike</Button>
+              </Col>
+              <Col className="text-center">
+                <Button onClick={this.like} style={{ width: '100%' }}>Like</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                {!this.state.loading && this.state.profiles.length > 0 &&
+                  <React.Fragment>
+                    {this.state.profiles[this.state.currentProfile].name}
+                    <br />
+                    {this.state.profiles[this.state.currentProfile].bio}
+                  </React.Fragment>
+                }
+              </Col>
+            </Row>
+            {/* <Row>
+              <Col>
+                <Jumbotron className="text-center">
+                  <h2>Prediction:</h2>
+                  <Spinner animation="grow" />
+                  <div style={{color:'red', fontSize:30}}>no match</div>
+                  <div style={{color:'green', fontSize:30}}>match</div>
+                </Jumbotron>
+              </Col>
+            </Row> */}
           </Col>
         </Row>
       </Container>
